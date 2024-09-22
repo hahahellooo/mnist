08ab0853f3b1fbdf2e67e35152286b0a5dcc22d1
hahahellooo <hahahello777@gmail.com>
2024-09-22 18:31:45 +0900

    Line test ok

--- src/mnist/worker.py
+++ src/mnist/worker.py
@@ -12,16 +12,18 @@ def run():
     connection = get_connection()
     with connection:
         with connection.cursor() as cursor:
-            sql = "SELELCT prediction_result FROM image_processing WHERE prediction_result IS NULL LIMIT 1"
+            sql = "SELECT  num, prediction_result FROM image_processing WHERE prediction_result IS NULL LIMIT 1"
             cursor.execute(sql)
             result = cursor.fetchone()
 
   # STEP 2
   # RANDOM 으로 0 ~ 9 중 하나 값을 prediction_result 컬럼에 업데이트
   # 동시에 prediction_model, prediction_time 도 업데이트
+    num = result['num']
     prediction_result = random.randint(0,9)
     prediction_model = 'RandomModel'
     prediction_time = datetime.now(timezone('Asia/Seoul')).strftime('%Y-%m-%d %H:%M:%S')
+    
     connection = get_connection()
     with connection:
         with connection.cursor() as cursor:
@@ -35,10 +37,9 @@ def run():
 
   # STEP 3
   # LINE 으로 처리 결과 전송
-   # curl -X POST -H 'Authorization: Bearer UuAPZM7msPnFaJt5wXTUx34JqYKO7n3AUlLq4b3eyZ4' -F 'message={{ dag_run.dag_id }} success' https://notify-api.line.me/api/notify
     KEY = os.environ.get('LINE_TOKEN')
     url = "https://notify-api.line.me/api/notify"
-    data = {"message":f"{prediction_result}를 성공적으로 저장했습니다!"}
+    data = {"message":f"이미지 {prediction_result}을/를 성공적으로 저장했습니다!"}
    # API 호출시 사용되는 헤더 정보
     headers={"Authorization":f"Bearer {KEY}"}
     response = requests.post(url, data, headers=headers)
