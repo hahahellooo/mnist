572baf61bf6d7629e0eb36d920a0328dffad25d3
hahahellooo <hahahello777@gmail.com>
2024-09-23 17:47:46 +0900

    please done

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # cron과 API를 동시에 테스트
-echo DB_IP=$DB_IP >> /etc/environment;
-echo LINE_TOKEN=$LINE_TOKEN >> /etc/environment;
+#echo DB_IP=$DB_IP >> /etc/environment;
+#echo LINE_TOKEN=$LINE_TOKEN >> /etc/environment;
+env >> /etc/environment;
 service cron start;uvicorn mnist.main:app --host 0.0.0.0 --port 8080 --reload
