a452e52480d137514422597003d7b28f84cb3881
hahahellooo <hahahello777@gmail.com>
2024-09-23 10:26:33 +0900

    fix syntax error

--- src/mnist/db.py
+++ src/mnist/db.py
@@ -1,18 +1,11 @@
 import pymysql.cursors
 
 def get_connection():
-  connection = pymysql.connect(host='localhost', port = 53306,
+
+    connection = pymysql.connect(host='localhost', port = 53306,
                             user = 'mnist', password = '1234',
                             database = 'mnistdb',
                             cursorclass=pymysql.cursors.DictCursor)
-  return connection
+    return connection
 
 
-def dml(sql, *values):
-  connection = get_connection()
-
-  with conn:
-    with conn.cursor() as cursor:
-        cursor.execute(sql, values)
-        conn.commit()
-        return cursor.rowcount
