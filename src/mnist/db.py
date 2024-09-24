12cf3490a9542baf1c1679aa3249e36567f416fb
hahahellooo <hahahello777@gmail.com>
2024-09-24 10:19:42 +0900

    int(IP)

--- src/mnist/db.py
+++ src/mnist/db.py
@@ -4,7 +4,7 @@ import os
 def get_connection():
 
     connection = pymysql.connect(host=os.getenv("DB_IP",'localhost'), 
-                                 port =os.getenv("DB_PORT", "53306"),
+                                 port =int(os.getenv("DB_PORT", "53306")),
                                  user = 'mnist', password = '1234',
                                  database = 'mnistdb',
                                  cursorclass=pymysql.cursors.DictCursor)
